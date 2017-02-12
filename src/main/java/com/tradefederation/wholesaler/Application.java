ce5e4a71a3df794520a818acf1f2dfac67c82811
Ryan Hoegg <ryan.hoegg@gmail.com>
2017-02-12 15:22:09 -0600

    Spruce up Swagger a bit

--- src/main/java/com/tradefederation/wholesaler/Application.java
+++ src/main/java/com/tradefederation/wholesaler/Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,12 +23,19 @@ public class Application {
     }
 
     @Bean
-    public Docket swaggerSettings() {
+    public Docket wholesalerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("wholesaler-api")
+                .apiInfo(wholesalerApiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
+                    .apis(RequestHandlerSelectors.basePackage("com.tradefederation.wholesaler"))
+                    .paths(PathSelectors.any())
+                    .build()
                 .pathMapping("/");
     }
-}
+
+    private ApiInfo wholesalerApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Wholesaler API")
+                .build();
+    }}
