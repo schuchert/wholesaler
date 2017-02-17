1cd800142e2fb859ce89f35bff644a2ea086cf96
Brett Schuchert <schuchert@yahoo.com>
2017-02-17 01:12:42 +0000

    Introduce lombok across most objects. Removed stale class.

--- src/main/java/com/tradefederation/wholesaler/inventory/Item.java
+++ src/main/java/com/tradefederation/wholesaler/inventory/Item.java
@@ -1,38 +1,16 @@
 package com.tradefederation.wholesaler.inventory;
 
 import com.tradefederation.wholesaler.retailer.Retailer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
 public class Item {
     public ItemId id;
     public ItemSpecification specification;
     public Retailer retailer;
     public BigDecimal price;
-
-    public Item() {
-    }
-
-    Item(ItemId id, ItemSpecification specification, Retailer retailer, BigDecimal price) {
-        this.id = id;
-        this.specification = specification;
-        this.retailer = retailer;
-        this.price = price;
-    }
-
-    public ItemId getId() {
-        return id;
-    }
-
-    public ItemSpecification getSpecification() {
-        return specification;
-    }
-
-    public Retailer getRetailer() {
-        return retailer;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
 }
--- src/main/java/com/tradefederation/wholesaler/inventory/ItemSpecificationId.java
+++ src/main/java/com/tradefederation/wholesaler/inventory/ItemSpecificationId.java
@@ -1,31 +1,17 @@
 package com.tradefederation.wholesaler.inventory;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ItemSpecificationId {
     public long id;
 
+    public ItemSpecificationId(String id) {
+        this.id = Long.valueOf(id);
+    }
+
     public ItemSpecificationId() {
-    }
-
-    public ItemSpecificationId(String value) {
-        id = Long.valueOf(value);
-    }
-
-    public ItemSpecificationId(long value) {
-        this.id = value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        ItemSpecificationId that = (ItemSpecificationId) o;
-
-        return id == that.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) (id ^ (id >>> 32));
     }
 }
