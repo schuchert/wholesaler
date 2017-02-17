b996ba538c9c639792779ec3f71b40327a292c27
Brett Schuchert <schuchert@yahoo.com>
2017-02-17 01:12:42 +0000

    Introduce lombok across most objects. Removed stale class.

--- src/main/java/com/tradefederation/wholesaler/endpoint/PurchaseRequest.java
+++
@@ -1,9 +0,0 @@
-package com.tradefederation.wholesaler.endpoint;
-
-import com.tradefederation.wholesaler.inventory.ItemSpecificationId;
-import com.tradefederation.wholesaler.retailer.RetailerId;
-
-public class PurchaseRequest {
-    public RetailerId retailerId;
-    public ItemSpecificationId itemSpecificationId;
-}
